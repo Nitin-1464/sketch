c21789f07337b3dc4beb4d62ac68acf0bc2e8219
= <=>
2023-03-06 15:56:48 +0530

    option

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const NewEL = (sel, prop) => Object.assign(document.createElement(sel), prop);
-
+//grid box creat
 const EL_grid = document.querySelector("#grid");
-const EL_clear = document.querySelector("#clear");
+ //Erase
+const EL_clear = document.querySelector("#erase");
 
 const EL_color = document.querySelector("[name=color]");
 
@@ -12,7 +13,9 @@ let color = "black";
 let isPenDown = false;
 
 function makeGrid() {
-  EL_grid.innerHTML = ""; // Clear current grid!
+  EL_grid.innerHTML = ""; 
+  
+  // Clear current grid!
   for (let i = 0; i < size ** 2; i++) {
     EL_grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     EL_grid.append(NewEL("div", {
@@ -29,12 +32,20 @@ function paint(EL) {
 }
 
 EL_clear.addEventListener("click", () => {
-  const tmp_color = color; // Remember current color
-  color = "transparent";   // Temporarily set it to transparent
+  const tmp_color = color; 
+  
+  // Remember current color
+  color = "transparent";  
+   
+  // Temporarily set it to transparent
   EL_grid.querySelectorAll(".box").forEach(paint); // Paint all cells as transparent
-  color = tmp_color;       //* Reset as it was before.
+  color = tmp_color;      
+  
+  
 });
 
+
+  //* Reset as it was before.
 EL_color.addEventListener("change", () => {
   color = EL_color.value;
   if (color === "random") color = `hsl(${~~(Math.random() * 360)}, 80%, 50%)`;
