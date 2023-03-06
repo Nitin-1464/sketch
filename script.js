c7c3fde05b81db6a8c12bbad629c83ad468adc27
= <=>
2023-03-06 15:26:12 +0530

    option

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+const NewEL = (sel, prop) => Object.assign(document.createElement(sel), prop);
 
-let text = "";
-for (let i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
+const EL_grid = document.querySelector("#grid");
+const EL_clear = document.querySelector("#clear");
+
+const EL_color = document.querySelector("[name=color]");
+
+const EL_size = document.querySelector("[name=size]");
+
+let size = parseInt(EL_size.value, 10);
+let color = "black";
+let isPenDown = false;
+
+function makeGrid() {
+  EL_grid.innerHTML = ""; // Clear current grid!
+  for (let i = 0; i < size ** 2; i++) {
+    EL_grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    EL_grid.append(NewEL("div", {
+      className: "box",
+      onmousedown()  { isPenDown = true; paint(this); },
+      onmouseup()    { isPenDown = false; },
+      onmouseenter() { if (isPenDown) paint(this); },
+    }));
+  }
+};
+
+function paint(EL) {
+  EL.style.backgroundColor = color;  
 }
 
-document.getElementById("vertically").innerHTML = text;
+EL_clear.addEventListener("click", () => {
+  const tmp_color = color; // Remember current color
+  color = "transparent";   // Temporarily set it to transparent
+  EL_grid.querySelectorAll(".box").forEach(paint); // Paint all cells as transparent
+  color = tmp_color;       //* Reset as it was before.
+});
+
+EL_color.addEventListener("change", () => {
+  color = EL_color.value;
+  if (color === "random") color = `hsl(${~~(Math.random() * 360)}, 80%, 50%)`;
+});
+
+EL_size.addEventListener("change", () => {
+  size = parseInt(EL_size.value, 10);
+  makeGrid();
+});
+
+// INIT!
+makeGrid();
